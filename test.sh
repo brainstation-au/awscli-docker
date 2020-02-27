5c236ca1e83201ac6068121b05320c147ad2773b
Khalid Saifullah <khalid@outlook.com.au>
2020-02-28 09:54:36 +1100

    clean up

--- test.sh
+++ test.sh
@@ -4,9 +4,8 @@ version=$(aws --version)
 
 echo $version
 
-if [[ "$version" == "aws-cli/$DOCKER_TAG"* ]]; then
-  echo "AWS CLI version matches with Docker Tag.";
-elif [[ "$DOCKER_TAG" == "latest" ]] && [[ "$version" == "aws-cli/1."* ]]; then
+if [[ "$version" == "aws-cli/$DOCKER_TAG"* ]] \
+  || ([[ "$DOCKER_TAG" == "latest" ]] && [[ "$version" == "aws-cli/1."* ]]); then
   echo "AWS CLI version matches with Docker Tag.";
 else
   echo "AWS CLI version doesn't match with Docker Tag '$DOCKER_TAG'"

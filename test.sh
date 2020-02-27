4c8cb8e322281e031034caefb889bed930783c90
Khalid Saifullah <khalid@outlook.com.au>
2020-02-27 23:43:47 +1100

    typo

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo $version
 
 if [[ "$version" == "aws-cli/$DOCKER_TAG"* ]]; then
   echo "AWS CLI version matches with Docker Tag.";
-elif [[ "$DOCKER_TAG" == "lateset" ]] && [[ "$version" == "aws-cli/1."* ]]; then
+elif [[ "$DOCKER_TAG" == "latest" ]] && [[ "$version" == "aws-cli/1."* ]]; then
   echo "AWS CLI version matches with Docker Tag.";
 else
   echo "AWS CLI version doesn't match with Docker Tag '$DOCKER_TAG'"

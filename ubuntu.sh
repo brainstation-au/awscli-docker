477498a1aae80e5e0ca16904d8dc410745ae3299
Khalid Saifullah <khalid@outlook.com.au>
2023-04-17 07:44:15 +1000

    use seperate script file for ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+apt-get update -y > /dev/null
+apt-get install -y curl unzip jq > /dev/null
+
+cd /tmp
+AARCH=$([[ "$TARGETPLATFORM" == "linux/arm64" ]] && echo "aarch64" || echo "x86_64")
+curl "https://awscli.amazonaws.com/awscli-exe-linux-${AARCH}-${DOCKER_TAG}.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install
+rm -rf aws
+rm awscliv2.zip
+
+apt-get purge --auto-remove unzip -y > /dev/null
+apt-get clean
